b750a8f16f3e701ec61a42da774501048903ae77
Bartosz Hendrych <bartek.hendrych@gmail.com>
2023-06-27 22:13:21 +0200

    Adding new css file

--- js/script.js
+++ js/script.js
@@ -1,14 +1,5 @@
 {
-    const tasks = [
-        {
-            content: "Pójść na Zakupy",
-            done: false,
-        },
-        {
-            content: "Zjeść Kolacje",
-            done: true,
-        },
-    ];
+    const tasks = [];
 
     const addNewTask = (newTaskContent) => {
         tasks.push({
